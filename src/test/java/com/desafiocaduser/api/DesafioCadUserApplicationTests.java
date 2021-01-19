3c540994e4a192e4f2b62a25bb6bbb52d6fd0a98
Alain <fabmendonca@hotmail.com>
2021-01-19 16:07:20 -0300

    Versão inicial

---
+++ src/test/java/com/desafiocaduser/api/DesafioCadUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiocaduser.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCadUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
